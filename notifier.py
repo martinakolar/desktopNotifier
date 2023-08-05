6a3b457257f401fd25335b4c933bc1b37df67248
Martina Kolar <martina.kolar7@gmail.com>
2023-08-05 21:09:35 +0200

    it works now!

--- notifier.py
+++ notifier.py
@@ -10,20 +10,17 @@ for file in os.listdir('.'):
         notification_icon_path = os.path.join(current_directory, file)
         print(f"The notification icon path is: {notification_icon_path}")
         
-        
-
-while(True):
-    #notification
-    notification.notify(
-        title = "Reminder to clean unnecessary files!",
-        message = "Maybe it's time for you to clean-up that prefetch, huh?",
-        app_icon = notification_icon_path,
-        # the notification stays for 30s
-        timeout = 30
-    )
-        
-    # shows every 5 days
-    # 60s * 60m * 24h * 5 days (shown in seconds)
-    time.sleep(60*60*24*5)
-
-        
+          
+    
+if __name__=="__main__":
+ 
+        notification.notify(
+            title = "Reminder to clean unnecessary files!",
+            message = "Maybe it's time for you to clean-up that prefetch, huh?",
+            app_icon = notification_icon_path,
+            # the notification stays for 30s
+            timeout = 30,
+)
+        # shows every 5 days
+        # 60s * 60m * 24h * 5 days (shown in seconds)
+        time.sleep(60*60*24*5)   
